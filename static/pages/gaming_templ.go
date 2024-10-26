05dae34bfaf9a9a29a187e87c512b50500ab4093
McCune1224 <alexmccune1224@gmail.com>
2024-10-26 13:16:24 -0400

    text for games completed

--- static/pages/gaming_templ.go
+++ static/pages/gaming_templ.go
@@ -59,7 +59,7 @@ func GamingPage() templ.Component {
 					}()
 				}
 				ctx = templ.InitializeContext(ctx)
-				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This game is an absolute gem. I was presented this on a beautiful  Christmas back in December of 2007 when I was 9 years old.  My dad had bought Valve’s amazing <a class=\"text-orange-400\" href=\"https://en.wikipedia.org/wiki/The_Orange_Box\">\"The Orange Box\" </a>Collection,  and while I now speculate my Dad had bought it so that  he could selfishly try out Team Fortress 2 as well as the Half-Life series,  he saw that Portal was a lower maturity rating of the two and would allow me to play that.  Besides the nostalgia aspect, Portal is just an amazing game in general.  The unsettling atmosphere of the game, you’re constantly under observation with cameras  tracking your every move within the chambers, the blurred windowed rooms above where you  can’t quite make out what’s on the other side looking in.  The music which would bounce between synthetic puzzle tracks,  to what I can only describe as a feeling of hauntingly vacant ambient music.  The only human voice you hear along the way throughout the game turns out to be a murderous AI who attempts to cook you alive.  The gameplay speaks for itself, with what feels like such a simple concept that builds on top of itself and continues to show you  more and more creative ways to utilize the concept of portals to solve the chambers and challenges within Aperture Science.</p>")
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This game is an absolute gem. I was presented this on a beautiful  Christmas back in December of 2007 when I was 9 years old.  My dad had bought Valve’s amazing <a class=\"text-orange-400\" href=\"https://en.wikipedia.org/wiki/The_Orange_Box\">\"The Orange Box\" </a>Collection,  and while I now speculate my Dad had bought it so that  he could selfishly try out Team Fortress 2 as well as the Half-Life series,  he saw that Portal was a lower maturity rating of the two and would allow me to play that.  Besides the nostalgia aspect, Portal is just an amazing game in general.  The unsettling atmosphere of the game, you’re constantly under observation with cameras  tracking your every move within the chambers, the blurred windowed rooms above where you  can’t quite make out what’s on the other side looking in.  The music which would bounce between synthetic puzzle tracks,  to what I can only describe as a feeling of hauntingly vacant ambient music.  The only human voice you hear along the way throughout the game  turns out to be a murderous AI who attempts to cook you alive.  The gameplay speaks for itself, with what feels like such a  simple concept that builds on top of itself and continues to show you  more and more creative ways to utilize the concept of portals to  solve the chambers and challenges within Aperture Science.</p>")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
@@ -81,7 +81,7 @@ func GamingPage() templ.Component {
 					}()
 				}
 				ctx = templ.InitializeContext(ctx)
-				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This was my very first game I ever played on PC. I absolutey love this game series, and the remaster that recently came out for it I was in love with. The visual asthetic of this game is something of its own. Something about seeing a simple trailed color light of your space fleet  as you command them around is just so satisfyting. My absolute favorite ship in this game was the Salvage Corvette, While my Dad and I do not see eye to eye, I can’t deny that some of my fondest memories as a kid  were watching / playing this with him. The encouragement he gave me for completing the resource managment  and strategy aspects of the game as well as the game world I would say domino my passion for SciFi, tech, and general interest in problem solving.</p>")
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This was my very first game I ever played on PC. I absolutey love this game series,  and the remaster that recently came out for it I was in love with. The visual asthetic of this game is something of its own.  Something about seeing a simple trailed color light of your space fleet  as you command them around is just so satisfyting. My absolute favorite ship in this game was the Salvage Corvette, Some of my fondest memories as a kid were watching and playing this with him.  The music, gameplay, and game world would domino my passion for SciFi, tech, and general interest in problem solving.</p>")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
@@ -91,39 +91,201 @@ func GamingPage() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Pokemon Emerald", "emerald_box.jpg").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var5 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This one was another childhood classic. Not much to add onto this besides what 50 other youtube videos could already say... Sableye is best, nothing further to add.</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Pokemon Emerald", "emerald_box.jpg").Render(templ.WithChildren(ctx, templ_7745c5c3_Var5), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Dragon Ball Z: Budokai Tenkaichi 3", "dbzbt3_box.jpg").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var6 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This was likely my gateway into fighting games as a genre. I don't consider this game itself to really be a fighting game, but growing up watching DragonballZ and then just  popping this game open to run some games with my neighborhood friends was amazing.</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Dragon Ball Z: Budokai Tenkaichi 3", "dbzbt3_box.jpg").Render(templ.WithChildren(ctx, templ_7745c5c3_Var6), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Team Fortress 2", "tf2_box.jpg").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var7 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This is likely the game I've played the most and still to this day.  While I got this from the Orange Box, I ended up not playing it early on  as it turns out being a 9 year old playing a rated M FPS menas you're playing against a lot of adults who are much better at aiming than you. Not to mention I never knew where to go on the maps and was lost very often....</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Team Fortress 2", "tf2_box.jpg").Render(templ.WithChildren(ctx, templ_7745c5c3_Var7), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Super Smash Bros. Ultimate", "ssbu_box.jpg").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var8 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">I've played basically all Smash games (besides 64) and it was tough to choose which one I liked most, but I think this one has to be it.  Gameplay wise I think Melee is much better, however, I have been in  the compeditive fighting game community (FGC) for over a decade now and I was lucky enough to get sponsored by a new / small eSports Org Sola Fida. I was lucky enough to get to travel to numerous FGC events (Evo 2019, Genesis 8, Frosty Fausting 2022, etc.)</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Super Smash Bros. Ultimate", "ssbu_box.jpg").Render(templ.WithChildren(ctx, templ_7745c5c3_Var8), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Dragon Ball FighterZ", "dbfz_box.jpg").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var9 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">This was my first game from <a href=\"https://en.wikipedia.org/wiki/Arc_System_Works\">Arc System Works</a> and my first of the fgc.</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Dragon Ball FighterZ", "dbfz_box.jpg").Render(templ.WithChildren(ctx, templ_7745c5c3_Var9), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Halo Reach", "reach_box.png").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var10 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">Martin O'Donnell carried my childhood.</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Halo Reach", "reach_box.png").Render(templ.WithChildren(ctx, templ_7745c5c3_Var10), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Minecraft", "minecraft_box.png").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var11 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">C418 also carried my Childhood.</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Minecraft", "minecraft_box.png").Render(templ.WithChildren(ctx, templ_7745c5c3_Var11), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Dark Souls 3", "ds3_box.jpg").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var12 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">The dark souls series is likely my favorite single player series. I was split between putting Elden Ring here or DS3, but I think DS3 has a bit more of a soft spot for me as I find it very tedious to start new characters and builds. I love the story for Elden Ring, but George R.R Martin's writing in this game is a mystery to me.</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Dark Souls 3", "ds3_box.jpg").Render(templ.WithChildren(ctx, templ_7745c5c3_Var12), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templates.GameCard("Metal Gear Solid V: The Phantom Pain", "mgsv_box.png").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Var13 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+				if !templ_7745c5c3_IsBuffer {
+					defer func() {
+						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+						if templ_7745c5c3_Err == nil {
+							templ_7745c5c3_Err = templ_7745c5c3_BufErr
+						}
+					}()
+				}
+				ctx = templ.InitializeContext(ctx)
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"content-center p-3 leading-relaxed text-left\">HIDEO KOJIMA IS MY GOAT. This game is so fucking fun holy shit.</p>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				return templ_7745c5c3_Err
+			})
+			templ_7745c5c3_Err = templates.GameCard("Metal Gear Solid V: The Phantom Pain", "mgsv_box.png").Render(templ.WithChildren(ctx, templ_7745c5c3_Var13), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
